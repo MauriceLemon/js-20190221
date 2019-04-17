946a6f0e83b0183b6125307f2416445771b80507
Maria Semionova <mauricelemon@my.smccd.edu>
2019-04-17 17:31:35 +0300

    Initialization of components moved down after render function call.

--- js/components/PhonesPage.js
+++ js/components/PhonesPage.js
@@ -1,48 +1,63 @@
 import Filter from './Filter.js';
 import ShoppingCart from './ShoppingCart.js';
 import PhonesCatalog from './PhonesCatalog.js';
+import PhoneViewer from './PhoneViewer.js';
 import { getAll, getById } from '../api/phones.js';
 
 export default class PhonesPage {
     constructor(element) {
         this.element = element;
 
-        this.render();
+        this.state = {
+          phones: getAll(),
+          selectedPhone: null,
+        };
 
-        this.initComponent(Filter);
-        this.initComponent(ShoppingCart);
-        this.initComponent(PhonesCatalog, {
-            phones: getAll(),
-        });
+        this.render();
     }
 
     initComponent(Constructor, props = {}) {
-        new Constructor(
-            this.element.querySelector(`[data-component="${Constructor.name}"]`),
-            props
-        );
+        const componentName = Constructor.name;
+        const element = this.element.querySelector(`[data-component="${Constructor.name}"]`);
+
+        if (element) {
+            new Constructor(element, props);
+        }
     }
 
     render() {
         this.element.innerHTML = `
-          <div class="row">
+            <div class="row">
+    
+              <!--Sidebar-->
+              <div class="col-md-2">
+                <section>
+                  <div data-component="Filter"></div>
+                </section>
+        
+                <section>
+                  <div data-component="ShoppingCart"></div>
+                </section>
+              </div>
+        
+              <!--Main content-->
+              <div class="col-md-10">
+              ${ this.state.selectedPhone ? `
+                <div data-component="PhoneViewer"></div>
+              ` : `
+                <div data-component="PhonesCatalog"></div>
+              `}
+              </div>
+            </div>
+        `;
 
-          <!--Sidebar-->
-          <div class="col-md-2">
-            <section>
-              <div data-component="Filter"></div>
-            </section>
-    
-            <section>
-              <div data-component="ShoppingCart"></div>
-            </section>
-          </div>
-    
-          <!--Main content-->
-          <div class="col-md-10">
-            <div data-component="PhonesCatalog"></div>
-          </div>
-    </div>
-    `;
+        this.initComponent(Filter);
+        this.initComponent(ShoppingCart);
+        this.initComponent(PhonesCatalog, {
+            phones: this.state.phones,
+        });
+        this.initComponent(PhoneViewer, {
+            phone: this.state.selectedPhone,
+        });
     }
 };
